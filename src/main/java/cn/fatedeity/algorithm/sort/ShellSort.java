7a9679bef5c54b077812735f1a274fb4c3f6fee1
fatedeity <799298142@qq.com>
2022-06-22 14:39:07 +0800

    feat: 希尔排序

---
+++ src/main/java/cn/fatedeity/algorithm/sort/ShellSort.java
@@ -0,0 +1,22 @@
+package cn.fatedeity.algorithm.sort;
+
+/**
+ * 希尔排序算法
+ */
+public class ShellSort {
+    public static int[] sort(int[] numbers) {
+        int length = numbers.length;
+        // 通常增量序列进行二分对原序列拆分
+        for (int gap = length >> 1; gap > 0; gap = gap >> 1) {
+            for (int i = gap; i < length; i++) {
+                int j = i, current = numbers[i];
+                while (j >= gap && numbers[j - gap] > current) {
+                    numbers[j] = numbers[j - gap];
+                    j = j - gap;
+                }
+                numbers[j] = current;
+            }
+        }
+        return numbers;
+    }
+}
